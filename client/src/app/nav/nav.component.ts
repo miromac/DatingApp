a1e0a9d43462f8d96a7f9ba021acec45849c0f9d
Miro Mac <MY_NAME@example.com>
2020-12-03 13:10:30 +0000

    End of section 5

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,14 +11,38 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
 
   model: any = {};
+  //currentUser$: Observable<User>;
+  //loggedIn: boolean;
 
-  constructor() { }
+  constructor(public acountService: AccountService) { }
 
   ngOnInit(): void {
+    //this.getCurrentUser();
+    //this.currentUser$ = this.acountService.currentUser$;
   }
 
   login() {
-    console.log(this.model);
+    this.acountService.login(this.model).subscribe(response => {
+      console.log(response);
+      //this.loggedIn = true;
+    }, error => {
+      console.log(error);
+    });
+    //console.log(this.model);
   }
+
+  logout(){
+    this.acountService.logout();
+    //this.loggedIn = false;
+  }
+
+  // getCurrentUser() {
+  //   this.acountService.currentUser$.subscribe(user => {
+  //     this.loggedIn = !!user;
+  //   }, error => {
+  //     console.log(error);
+  //   })
+  //}
+  // <ul class="navbar-nav mr-auto" *ngIf="loggedIn">
 
 }
