0c193e3d026db370dee278cba6c17750d3e333e3
Miro Mac <itcodevelopment@gmail.com>
2022-11-28 14:07:01 +0200

    API updated to .Net 7

--- API/Extensions/DateTimeExtensions.cs
+++ API/Extensions/DateTimeExtensions.cs
@@ -4,12 +4,13 @@ namespace API.Extensions
 {
     public static class DateTimeExtensions
     {
-        public static int CalculateAge(this DateTime dob)
+        public static int CalculateAge(this DateOnly dob)
         {
-            var today = DateTime.Today;
-            var age = today.Year - dob.Year;
-            if (dob.Date > today.AddYears(-age)) age--;
-            return age;
+        var today = DateOnly.FromDateTime(DateTime.UtcNow);
+        var age = today.Year - dob.Year;
+        if (dob > today.AddYears(-age)) age--;
+        return age;
         }
+
     }
 }
